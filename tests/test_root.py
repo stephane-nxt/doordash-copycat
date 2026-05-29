dfdaced9eff1a481afcd165fe3abefa99c70ff79
Stephane <stephane@nexthope.net>
2026-05-29 16:36:05 +0300

    [test]  ad testing

---
+++ tests/test_root.py
@@ -0,0 +1,6 @@
+import httpx
+
+def test_root():
+    response = httpx.get("http://localhost:8000/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the DoorDash Delivery Fee Service API"}
---
+++ tests/test_status.py
@@ -0,0 +1,6 @@
+import httpx
+
+def test_status():
+    response = httpx.get("http://localhost:8000/status/")
+    assert response.status_code == 200
+    assert response.json() == {"status": "Service is up and running"}
